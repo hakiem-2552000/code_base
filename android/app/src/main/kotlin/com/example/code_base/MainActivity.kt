2ca7601ff3c9c1b237650b5256a627827375a5dc
hybrid-technologies-vn <cungtrang0978@gmail.com>
2022-01-12 12:17:22 +0700

    feat: config android

--- android/app/src/main/kotlin/com/example/code_base/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/code_base/MainActivity.kt
@@ -1,6 +1,17 @@
 package com.example.code_base
 
-import io.flutter.embedding.android.FlutterActivity
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterFragmentActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+import io.flutter.plugin.common.MethodChannel
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterFragmentActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, "flavor").setMethodCallHandler { call, result ->
+            result.success(BuildConfig.FLAVOR)
+        }
+    }
 }
