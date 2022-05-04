8ec54a3b4c275c511adbe57bcbb325f5f0262434
cungtrang0978 <cungtrang0978@gmail.com>
2022-05-04 16:52:16 +0700

    iOS: add flavor, name with dev, stg, prod env

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,6 +8,17 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+    if let controller = window.rootViewController as? FlutterViewController {
+        print("Init flavor")
+        let flavorChannel = FlutterMethodChannel(
+            name: "flavor",
+            binaryMessenger: controller.binaryMessenger)
+        flavorChannel.setMethodCallHandler({(call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
+            // Note: this method is invoked on the UI thread
+            let flavor = Bundle.main.infoDictionary?["AppFlavor"]
+                result(flavor)
+        })
+    }
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
